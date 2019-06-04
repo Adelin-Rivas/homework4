a01cf64c9eb53c8e4eb5ff0e4a0afe099fb83bfd
cd \ruby\devkit <adelin300@aol.com>
2019-06-04 00:41:33 -0400

    added fuction

--- index.js
+++ index.js
@@ -1,24 +1,85 @@
-let tableName=document.getElementById("table");
-let addRow=document.getElementById("addRow");
 
-console.log(tableName);
+let addRow=document.getElementById("addRow");
+let tableName=document.getElementById("table");
+let addColum=document.getElementById("addColum");
+let delRow=document.getElementById("delR");
+let delCol=document.getElementById("delC")
+
+
+ 
 addRow.addEventListener("click", rowHandler);
+addColum.addEventListener("click", colHandler);
+delRow.addEventListener("click", delR);
+delCol.addEventListener("click", delC);
+
+
+
+function delC()
+{
+  let rowSize=tableName.rows.length;
+  
+    
+  for(let i=0;i<rowSize; i++)
+  {
+    let rowlocation = tableName.rows[i];
+    
+      rowlocation.deleteCell(0)
+     
+  }
+
+}
+function delR()
+{
+   tableName.deleteRow(-1);
+
+}
+
+
+
+function colHandler()
+{
+ 
+
+    let rowSize=tableName.rows.length;
+  
+    
+    for(let i=0;i<rowSize; i++)
+    {
+      let rowlocation = tableName.rows[i];
+      
+      let cell= rowlocation.insertCell(-1);
+      cell.innerHTML=" "+i;
+  }
+
+}
+
+
 
 function rowHandler()
 
 {
-      
+  
+   
+    let rowSize=tableName.rows[0].cells.length;
+    let row= tableName.insertRow(-1)
     
-    let rowSize=tableName.rows.length;
-     console.log(rowSize);
+    for(let i=0; i<rowSize; i++)
      
-     for(let i=0;i<rowSize; i++)
-     {
-       let rowlocation = tableName.rows[i];
-       console.log("--->" + rowlocation);
-       let cell= rowlocation.insertCell(0);
-       cell.innerHTML="i";
+      {
+
+        cell= document.createElement("td");
+        cell.id=""+i+" ";
+        let text= document.createTextNode(" "+i);
+        cell.appendChild(text);
+        row.appendChild(cell);
+
+      }
+      
+      
+      
+     
+
+
      }
      
 
-}
